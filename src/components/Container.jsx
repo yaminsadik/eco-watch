ac9a8a0fac56c10f74e22b1b671acb96b0d8a509
Sadik Yamin <yaminsadik.sy@gmail.com>
2024-07-08 22:53:31 -0500

    Navbar and Footer Added

---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+export default function Container({ children }) {
+  return (
+    <div className="md:w-11/12 w-full md:px-0 px-3 mx-auto">
+      {children}
+    </div>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from "../assets/logo.png";
+import Container from "./Container";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#212121] py-10 text-white">
+      <Container>
+        <div className="flex flex-col items-center sm:flex-row justify-between">
+          <img src={logo} alt="Company Logo" className="w-36 h-10 mb-4 sm:mb-0" />
+          <ul className="flex flex-col sm:flex-row items-center gap-4">
+            <li><a href="#" className="hover:underline">About Us</a></li>
+            <li><a href="#" className="hover:underline">Contact Us</a></li>
+            <li><a href="#" className="hover:underline">Privacy Policy</a></li>
+            <li><a href="#" className="hover:underline">FAQ</a></li>
+          </ul>
+        </div>
+      </Container>
+    </footer>
+  );
+}
