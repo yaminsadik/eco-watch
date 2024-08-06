447810bb47c87e9661e1cb10c33b972f8a8313e9
Sadik Yamin <yaminsadik.sy@gmail.com>
2024-08-06 15:57:49 -0500

    Updated Readme

--- src/components/Home/Front.jsx
+++ src/components/Home/Front.jsx
@@ -9,7 +9,7 @@ const Front = () => {
           className={`py-8 sm:rounded-3xl relative w-full h-[820px] bg-homefrontbg bg-cover lg:bg-center bg-scroll bg-no-repeat bg-left`}
         ></div>
       </div>
-      <article className="lg:flex hidden items-end absolute top-[40%] right-0 w-[30%] h-1/2 px-2">
+      <article className="lg:flex hidden items-end absolute top-[50%] right-0 w-[30%] h-1/2 px-2">
         <div className="text-white text-[26px] w-[80%]">
           Find, locate and clean the trash in your area
         </div>
